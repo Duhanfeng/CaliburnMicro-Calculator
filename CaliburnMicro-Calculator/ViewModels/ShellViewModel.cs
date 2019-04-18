544e4da1d31d8aa27e4d31f16cda7b661ed46d61
陈晓聪 <a1194457876@outlook.com>
2019-04-19 01:17:54 +0800

    添加项目文件。

---
+++ CaliburnMicro-Calculator/ViewModels/ShellViewModel.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel;
+using System.Threading;
+using System.Windows;
+using System.Windows.Controls;
+using Caliburn.Micro;
+
+namespace CaliburnMicro_Calculator.ViewModels
+{
+    public class ShellViewModel : Conductor<object>
+    {
+        public ShellViewModel()
+        {
+        }
+        public void ShowCalculator()
+        {
+            ActivateItem(new CalculatorViewModel());
+        }
+    }
+}
